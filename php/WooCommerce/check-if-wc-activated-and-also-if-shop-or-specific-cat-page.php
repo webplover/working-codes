e2803b7f84254f63cf48c1d9f3d4aeb33a3a8910
WebPlover <76245561+webplover@users.noreply.github.com>
2022-03-12 10:26:55 +0500

    Create check-if-wc-activated-and-also-if-shop-or-specific-cat-page.php

---
+++ php/WooCommerce/check-if-wc-activated-and-also-if-shop-or-specific-cat-page.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * If woocommerce activated, and also if shop or specefic category page
+ */
+
+add_action('wp_footer', function () {
+    if (class_exists('woocommerce')) {
+        if (is_shop() || is_product_category('category_slug')) { // Or pass array for multiple categories is_product_category(['first', 'second'])
+            echo 'this is shop page';
+        }
+    }
+});
