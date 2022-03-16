0e38233230f0748709f9f5f1e653dbffaad7b786
WebPlover <76245561+webplover@users.noreply.github.com>
2022-03-16 09:31:23 +0500

    Update MutationObserver.js

--- js/MutationObserver.js
+++ js/MutationObserver.js
@@ -1,13 +1,40 @@
-const observer = new MutationObserver((mutationsList) => {
-  for (let mutation of mutationsList) {
-    if (mutation.type === "childList") {
-      // Do Something...
-      console.log(866);
-    }
-  }
-});
 
-observer.observe(document.querySelector("#gform_wrapper_1"), {
-  childList: true,
-  // subtree: true
-});
+  /**
+   * Detect Child Elements (added or removed)
+   */
+
+
+
+const observer = new MutationObserver((mutationsList) => {
+    for (let mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        // Do Something...
+        console.log(866);
+      }
+    }
+  });
+  
+  observer.observe(document.querySelector("#gform_wrapper_1"), {
+    childList: true,
+    // subtree: true
+  });
+  
+
+  /**
+   * Detect Elements attributes changes
+   */
+
+   const observer = new MutationObserver((mutationsList) => {
+    for (let mutation of mutationsList) {
+      if (mutation.type === "attributes") {
+        // Do Something...
+        console.log(866);
+      }
+    }
+  });
+
+  observer.observe(document.querySelector("#gform_wrapper_1"), {
+    childList: true,
+    attributes: true,
+    // subtree: true
+  });
