b99ebcd3b7a0ea8991776ab6825701769703bf96
WebPlover <76245561+webplover@users.noreply.github.com>
2022-11-03 12:04:09 +0500

    Update get_fb_ids_from_fb_search.js

--- console-scraping/get_fb_ids_from_fb_search.js
+++ console-scraping/get_fb_ids_from_fb_search.js
@@ -3,7 +3,7 @@ var links = [];
 // Get group links
 document
   .querySelectorAll(
-    ".qi72231t.nu7423ey.n3hqoq4p.r86q59rh.b3qcqh3k.fq87ekyn.bdao358l.fsf7x5fv.rse6dlih.s5oniofx.m8h3af8h.l7ghb35v.kjdc1dyq.kmwttqpk.srn514ro.oxkhqvkx.rl78xhln.nch0832m.cr00lzj9.rn8ck1ys.s3jn8y49.icdlwmnq.cxfqmxzd.pbevjfx6.innypi6y"
+    'div[aria-label="Search results"] a[href*="/groups/"][tabindex="0"]'
   )
   .forEach((item) => {
     links.push(item.getAttribute("href"));
