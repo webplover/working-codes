a3617c19193ea57ad56b9bb266db7482978f9fa3
WebPlover <76245561+webplover@users.noreply.github.com>
2022-11-13 12:41:56 +0500

    Update get-diff-in-days-between-two-times.php

--- php/get-diff-in-days-between-two-times.php
+++ php/get-diff-in-days-between-two-times.php
@@ -3,9 +3,10 @@
 // Get diff in days, between two dates
 
 $date1 = 'July 21, 2022';
-$date2 = 'October 19, 2022';
+$date2 = date("F j, Y");
 
 $diff = strtotime($date1) - strtotime($date2);
+$oprator =  strtotime($date1) > strtotime($date2) ? '-' : '';
 $days =  abs(round($diff / 86400));
 
-echo "$days Days";
+echo "$oprator$days Days";
