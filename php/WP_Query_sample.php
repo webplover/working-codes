94441976e1ddb6b148bf0ed087aeb3d7608bdecc
WebPlover <76245561+webplover@users.noreply.github.com>
2023-01-05 11:58:09 +0500

    Update WP_Query_sample.php

--- php/WP_Query_sample.php
+++ php/WP_Query_sample.php
@@ -1,8 +1,8 @@
 <?php
-$query = new WP_Query(['post_type' => 'product']);
-if ($query->have_posts()) {
-    while ($query->have_posts()) {
-        $query->the_post();
-    }
+$query = new WP_Query(['post_type' => 'product', 'posts_per_page' => -1]);
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+        }
 }
 wp_reset_query();
