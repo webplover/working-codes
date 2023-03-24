03d44c62de62d96142d824e3d0e29884a5e06b8d
WebPlover <76245561+webplover@users.noreply.github.com>
2023-03-24 12:22:32 +0500

    Create node-woocommerce-api.js

---
+++ js/node-woocommerce-api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const data = {
+  regular_price: "12",
+  sale_price: "9.00",
+};
+
+(async function () {
+  // GET Request
+  let response1 = await axios.put(
+    "https://onlydev.ml/site2/wp-json/wc/v3/products/16/variations/17",
+    {
+      auth: {
+        username: "ck_12345678910",
+        password: "cs_12345678910",
+      },
+    }
+  );
+
+  // PUT Request
+  let response2 = await axios.put(
+    "https://onlydev.ml/site2/wp-json/wc/v3/products/16/variations/17",
+    data,
+    {
+      auth: {
+        username: "ck_12345678910",
+        password: "cs_12345678910",
+      },
+    }
+  );
+})();
