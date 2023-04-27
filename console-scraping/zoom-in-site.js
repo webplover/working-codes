bca2d8db96a812f8d0d07c943d4777d8de625297
WebPlover <76245561+webplover@users.noreply.github.com>
2023-04-27 08:13:02 +0500

    Create zoom-in-site.js

---
+++ console-scraping/zoom-in-site.js
@@ -0,0 +1,12 @@
+document.head.insertAdjacentHTML('beforeend', `
+<style>
+body {
+    zoom: 200% !important;
+}
+
+nav[class*="Navbar_container"],
+[class*="ContextMenu_container"] {
+    display: none !important;
+}
+</style>
+`);
