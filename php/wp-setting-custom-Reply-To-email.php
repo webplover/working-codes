9637db2e8233f21bed2308588d687d3ce20fc75b
WebPlover <76245561+webplover@users.noreply.github.com>
2024-04-19 10:32:40 +0500

    Create wp-setting-custom-Reply-To-email.php

---
+++ php/wp-setting-custom-Reply-To-email.php
@@ -0,0 +1,27 @@
+<?php
+add_filter(
+  'wp_mail',
+  function ($args) {
+    $reply_to = 'Reply-To: userdd1@goko.com'; // Your custom Reply-To address
+
+    if (!empty($args['headers'])) {
+      if (!is_array($args['headers'])) {
+        // Convert headers string to an array
+        $args['headers'] = array_filter(explode("\n", str_replace("\r\n", "\n", $args['headers'])));
+      }
+
+      // Remove any existing Reply-To headers
+      $args['headers'] = array_filter($args['headers'], function ($header) {
+        return strpos(strtolower($header), 'reply-to:') !== 0;
+      });
+    } else {
+      $args['headers'] = [];
+    }
+
+    // Add the new Reply-To header
+    $args['headers'][] = $reply_to;
+
+    return $args;
+  },
+  PHP_INT_MAX // Ensures this runs last
+);
