7e19911669a34db132b2b754d0e22149ec896242
WebPlover <76245561+webplover@users.noreply.github.com>
2024-06-02 08:37:26 +0500

    Update same-width-elements.js

--- js/same-width-elements.js
+++ js/same-width-elements.js
@@ -1,8 +1,10 @@
-function setMaxWidth(selector) {
-  const elements = [...document.querySelectorAll(selector)];
-  const maxWidth = Math.max(...elements.map((el) => el.offsetWidth));
-  elements.forEach((el) => (el.style.width = `${maxWidth}px`));
-}
+document.addEventListener("DOMContentLoaded", () => {
+  function setMaxWidth(selector) {
+    const elements = [...document.querySelectorAll(selector)];
+    const maxWidth = Math.max(...elements.map((el) => el.offsetWidth));
+    elements.forEach((el) => (el.style.width = `${maxWidth}px`));
+  }
 
-// Call the function for different selectors
-setMaxWidth(".my-box");
+  // Call the function for different selectors
+  setMaxWidth(".my-box");
+});
