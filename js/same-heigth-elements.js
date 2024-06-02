3052bc0a7e139efa6a4c023f0530a42562a5079d
WebPlover <76245561+webplover@users.noreply.github.com>
2024-06-02 08:37:53 +0500

    Update same-heigth-elements.js

--- js/same-heigth-elements.js
+++ js/same-heigth-elements.js
@@ -1,8 +1,10 @@
-function setMaxHeight(selector) {
-  const elements = [...document.querySelectorAll(selector)];
-  const maxHeight = Math.max(...elements.map((el) => el.offsetHeight));
-  elements.forEach((el) => (el.style.height = `${maxHeight}px`));
-}
+document.addEventListener("DOMContentLoaded", () => {
+  function setMaxHeight(selector) {
+    const elements = [...document.querySelectorAll(selector)];
+    const maxHeight = Math.max(...elements.map((el) => el.offsetHeight));
+    elements.forEach((el) => (el.style.height = `${maxHeight}px`));
+  }
 
-// Call the function for different selectors
-setMaxHeight(".my-box");
+  // Call the function for different selectors
+  setMaxHeight(".my-box");
+});
