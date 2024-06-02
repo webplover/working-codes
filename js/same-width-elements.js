0a5e4daedfd47015c08d042f93e28d9af3068c36
WebPlover <76245561+webplover@users.noreply.github.com>
2024-06-02 08:33:39 +0500

    Update and rename same-height-elements.js to same-width-elements.js

---
+++ js/same-width-elements.js
@@ -0,0 +1,8 @@
+function setMaxWidth(selector) {
+  const elements = [...document.querySelectorAll(selector)];
+  const maxWidth = Math.max(...elements.map((el) => el.offsetWidth));
+  elements.forEach((el) => (el.style.width = `${maxWidth}px`));
+}
+
+// Call the function for different selectors
+setMaxWidth(".my-box");
