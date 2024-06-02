37d7b194ff1178842d5617e88e27744b0b927c42
WebPlover <76245561+webplover@users.noreply.github.com>
2024-06-02 08:34:38 +0500

    Create same-heigth-elements.js

---
+++ js/same-heigth-elements.js
@@ -0,0 +1,8 @@
+function setMaxHeight(selector) {
+  const elements = [...document.querySelectorAll(selector)];
+  const maxHeight = Math.max(...elements.map((el) => el.offsetHeight));
+  elements.forEach((el) => (el.style.height = `${maxHeight}px`));
+}
+
+// Call the function for different selectors
+setMaxHeight(".my-box");
