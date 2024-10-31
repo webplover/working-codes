91094679625f93d281d09a5274507e7f98d059be
WebPlover <76245561+webplover@users.noreply.github.com>
2024-10-31 08:39:01 +0500

    Create jquery_dom_ready.js

---
+++ js/jquery_dom_ready.js
@@ -0,0 +1,4 @@
+// Waits for the DOM to load, using $ as a local alias for jQuery to avoid conflicts with other libraries
+jQuery(function($) {
+    // Your code here, safely using $ for jQuery operations
+});
